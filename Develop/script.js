fa5c31ea8f47efd44da8e55f1ebc8b2bf8bea941
Logan Merchant <merchantclogan@gmail.com>
2020-08-14 21:44:34 -0600

    add arrays and link function to button click

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,22 @@
 // Show password on the page. 
 
 // Assignment code here
+var alphabetInfo = [
+  a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+];
+
+var numericInfo = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+]
+
+var specialCharInfo = [
+  " ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+","-",".","/",":",";","<","=",">","?","@","[", "]", "^", "_", '`', "{", "|", "}", "~"
+]
+
+var generatePassword = function() {
+  window.prompt("Welcome to the password generator! How many characters is this password supposed to be in length?");
+  return "Hello";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
